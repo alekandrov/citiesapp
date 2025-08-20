fcb20d4c6e622e6e619a3a0f5c35aef8d904450b
alekandro <>
2025-08-20 02:26:44 -0300

    Added country flag in City Detail

--- CitiesApp/Features/CityDetail/View/CityInfoView.swift
+++ CitiesApp/Features/CityDetail/View/CityInfoView.swift
@@ -2,8 +2,27 @@ import SwiftUI
 
 struct CityInfoView: View {
     let city: City
+    
+    @ViewBuilder
+    private func CountryFlag() -> some View {
+        HStack {
+            Spacer()
+            AsyncImage(url: URL(string: "https://flaglog.com/codes/standardized-rectangle-120px/\(city.country).png")) { image in
+                image
+                    .resizable()
+                    .frame(width: 180, height: 120)
+                    .scaledToFit()
+                    .border(Color.gray)
+            } placeholder: {
+                ProgressView()
+            }
+            Spacer()
+        }.padding(.top, 16)
+    }
+    
     var body: some View {
         Form {
+            Section(header: CountryFlag()) {}
             Section(header: Text(String(localized: "city"))) {
                 HStack { Text(String(localized: "name")); Spacer(); Text(city.name) }
                 HStack { Text(String(localized: "country")); Spacer(); Text(city.country) }
